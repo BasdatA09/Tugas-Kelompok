7cff252fcdd19cafe40edbb6c2750e5f7ef15431
Satria Bagus <satria_bagus@hotmail.com>
2016-11-25 23:00:35 +0700

    menambahkan halaman home untuk dosen

---
+++ Front-End/home-dosen-view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SiSidang</title>
+		<link rel="stylesheet" href="assets/css/vendor.css" />
+		<link rel="stylesheet" href="assets/css/app.css" />
+	</head>
+	<body>
+		<?php include 'header.php' ?>
+		<div class="row homePage">
+				<?php include 'home-dosen.php' ?>
+		</div>
+		<?php include 'footer.php' ?>
+		<?php include 'js.php' ?>
+	</body>
+</html>
