88e5f5d0bdd57e0b95d8fe5f30bc56cf2081ea4d
Rafiano Rizky Rubiantoro <rafi804@gmail.com>
2016-12-03 15:20:36 +0700

    add mks-table-dosen

---
+++ mks-table-dosen.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+require_once 'database.php';
+
+function get_table($nipdosen)
+{
+    $connection = new database();
+    $conn = $connection->connectDB();
+
+    try {
+        $query = 'SELECT DISTINCT mks.idmks, mks.judul, mhs.nama, mks.tahun, mks.semester, jmks.namamks,
+			            mks.ijinmajusidang, mks.pengumpulanhardcopy, mks.issiapsidang
+                  FROM mahasiswa mhs NATURAL JOIN mata_kuliah_spesial mks JOIN jenismks jmks ON mks.idjenismks = jmks.id,
+                      dosen_pembimbing dpb, dosen_penguji dpj
+                  WHERE (dpb.idmks = mks.idmks and dpb.nipdosenpembimbing = :nipdosen) OR 
+                      (dpj.idmks = mks.idmks and dpj.nipdosenpenguji = :nipdosen);';
+
+        $hasil = $conn->prepare($query);
+        $hasil->execute(array(':nipdosen'=>$nipdosen));
+
+        while($hasil_row = $hasil->fetch(PDO::FETCH_ASSOC)) {
+            echo '<tr>
+                    <td>' . $hasil_row['idmks'] . '</td>
+                    <td>' . $hasil_row['judul'] . '</td>        
+                	<td>' . $hasil_row['nama']. '</td>';
+            if ($hasil_row['semester'] == 1) {
+                echo '<td> Gasal <br>' . $hasil_row['tahun'] . '/' . ($hasil_row['tahun']+1) . '</td>';
+            } elseif ($hasil_row['semester'] == 2) {
+                echo '<td> Genap <br>' . $hasil_row['tahun'] . '/' . ($hasil_row['tahun']-1) . '</td>';
+            } else {
+                echo '<td> Pendek <br>' . $hasil_row['tahun'] . '/' . ($hasil_row['tahun']-1) . '</td>';
+            }
+
+            echo '<td>' . $hasil_row['namamks'] . '</td>
+				  <td><ul>';
+
+            if ($hasil_row['ijinmajusidang']) {
+                echo '<li>Izin Masuk Sidang</li>';
+            }
+            if ($hasil_row['pengumpulanhardcopy']) {
+                echo '<li>Kumpul Hard Copy</li>';
+            }
+            if ($hasil_row['issiapsidang']) {
+                echo '<li>Siap Sidang</li>';
+            }
+
+            echo '</ul></td></tr>';
+        }
+
+    } catch (PDOException $e){
+        echo $e->getMessage();
+    }
+}
+
+?>
+<!-- Schedule Table Module -->
+<div class="small-12 columns">
+    <h1 class="subtitle">Daftar Mata Kuliah Spesial (dosen)</h1>
+    <button class="addScheduleButton" id="admAddScheduleButton" href="tambah-mks.php">Tambah</button>
+    <table id="daftarMKS">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Judul</th>
+            <th>Mahasiswa</th>
+            <th>Term</th>
+            <th>Jenis MKS</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        get_table();
+        ?>
+
+        <script>
+            $(document).ready(function() {
+
+                $('#daftarMKS').DataTable();
+            } );
+        </script>
+
+        </tbody>
+    </table>
+</div>
